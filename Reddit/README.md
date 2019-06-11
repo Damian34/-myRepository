248184ba38e9806d4b5a6a5ae54b62bc0a578cc0
Damian34 <damian1322@interia.pl>
2019-06-11 10:01:07 +0200

    remove

--- Reddit/README.md
+++
@@ -1,6 +0,0 @@
-
-Sample Java Projects
-====================
-
-This Folder contains some task projects done based on
-https://www.reddit.com/r/dailyprogrammer/
--- Reddit/TalkingClock/src/talkingclock/Main.java
+++
@@ -1,9 +0,0 @@
-package talkingclock;
-
-public class Main {
-
-    public static void main(String[] args) {
-        TalkingClock clock = new TalkingClock();
-        clock.talk("23:05");
-    }
-}
