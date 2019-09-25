90698b0364ed8ba2f8cec8a921a80afe0cc26bc9
Damian34 <damian1322@interia.pl>
2019-09-25 22:39:05 +0200

    Application using Allegro REST API in console app

---
+++ AllegroRestApi/OrdersApp/src/frames/operations/ConfigFrame.java
@@ -0,0 +1,27 @@
+package frames.operations;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class ConfigFrame {
+
+    public ConfigFrame(){}
+    
+    //classic windows frame
+    public ConfigFrame(JFrame fr,String Title)
+    {
+        fr.setTitle(Title);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        fr.setLocation(dim.width/2-fr.getSize().width/2, dim.height/2-fr.getSize().height/2);
+        fr.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("icon.png")));  
+        try {
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        SwingUtilities.updateComponentTreeUI(fr);
+        fr.pack();
+        } 
+        catch (Exception e) {}
+    }
+}
---
+++ AllegroRestApi/README.md
@@ -0,0 +1,7 @@
+
+AllegroRestApi
+====================
+
+The application uses Allegro REST API. You must set the file config.txt before running program.
+the application retrieves and displays payment data in the table, among others, payment method, status, amount, date and others
+
