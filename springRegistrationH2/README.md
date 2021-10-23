a0c2235bd978d8db4d7c3f11d2b561bc1a13e92d
Damian34 <damian1322@interia.pl>
2021-10-23 10:48:10 +0200

    spring registration app with H2 database, read more in  README.md

---
+++ springRegistrationH2/README.md
@@ -0,0 +1,19 @@
+Spring Registration
+====================
+App is an example of registration in Spring.
+Application allows you to log in, register the user and store notes with spring protection 
+
+dependencies included:
+- Spring Web
+- Spring Security
+- Spring Data JPA
+- Thymeleaf
+- H2 Database
+
+ H2 is in-memory database and volatile, then data will be lost when we restart the application. 
+ but also there is no need for installing any SQL DB in order to run the application
+ 
+ contains also:
+ - spring login
+ - registration
+ - 2 tables Note, Users
---
+++ springRegistrationH2/src/main/java/example/registration/table/Note.java
@@ -0,0 +1,50 @@
+package example.registration.table;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String id_user;
+    private String title;
+    private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(String id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
